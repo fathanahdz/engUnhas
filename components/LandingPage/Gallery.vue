7fd07274b42a00e3536d18335bc5c6d79e2c6798
RandyTunru <randytunru@gmail.com>
2023-10-25 22:51:31 +0800

    added padding in Galeri

--- components/LandingPage/Gallery.vue
+++ components/LandingPage/Gallery.vue
@@ -5,13 +5,13 @@
             <div class="flex relative items-center justify-center h-full w-full">
                 <h1 class="py-2 px-5 bg-grey text-xl font-bold text-center text-blue">GALERI</h1>
             </div>
-            <div class="flex flex-row gap-6 overflow-y-auto scrollbar-hide mt-6">
-                <img loading="lazy" src="@/assets/images/img-gallery1.png" alt="Galeri1">
-                <img loading="lazy" src="@/assets/images/img-gallery2.png" alt="Galeri2">
-                <img loading="lazy" src="@/assets/images/img-gallery3.png" alt="Galeri3">
-                <img loading="lazy" src="@/assets/images/img-gallery1.png" alt="Galeri1">
-                <img loading="lazy" src="@/assets/images/img-gallery2.png" alt="Galeri2">
-                <img loading="lazy" src="@/assets/images/img-gallery3.png" alt="Galeri3">
+            <div class="flex flex-row gap-6 overflow-y-auto scrollbar-hide mt-6 px-4">
+                <img loading="lazy" src="@/assets/images/img-gallery1.png" alt="Galeri1" class="max-w-[45vw] object-contain">
+                <img loading="lazy" src="@/assets/images/img-gallery2.png" alt="Galeri2" class="max-w-[45vw] object-contain">
+                <img loading="lazy" src="@/assets/images/img-gallery3.png" alt="Galeri3" class="max-w-[45vw] object-contain">
+                <img loading="lazy" src="@/assets/images/img-gallery1.png" alt="Galeri1" class="max-w-[45vw] object-contain">
+                <img loading="lazy" src="@/assets/images/img-gallery2.png" alt="Galeri2" class="max-w-[45vw] object-contain">
+                <img loading="lazy" src="@/assets/images/img-gallery3.png" alt="Galeri3" class="max-w-[45vw] object-contain">
             </div>
         </section>
     </div>
